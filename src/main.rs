37b6f56137719f73970e340ba05a48b136ee965f
Jonathan Hale <promotion.artwork@gmail.com>
2024-04-11 12:01:15 -0600

    did the mut and shadowing example

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+
+    let t = 5;
+    let t = t + 1;
+    let t = t * 2;
+    println!("The vakue of t is {}", t);
+}
