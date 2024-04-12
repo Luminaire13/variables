de0617e6763455af71a1ca145833e08ee9018886
Jonathan Hale <promotion.artwork@gmail.com>
2024-04-12 17:28:13 -0600

    math operator test

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,30 @@ fn main() {
     x = 6;
     println!("The value of x is: {}", x);
 
-    const MAX_POINTS: u32 = 100_000;
+    //const MAX_POINTS: u32 = 100_000;
 
     let t = 5;
     let t = t + 1;
     let t = t * 2;
-    println!("The vakue of t is {}", t);
+    println!("The value of t is {}", t);
+
+    let spaces = "  ";
+    let spaces = spaces.len();
+
+    println!("Spaces:{}",spaces);
+
+    //let mut spaces = "  ";
+    //spaces = spaces.len();
+    //
+    let guess: u16  = "42".parse().expect("Not a number!");
+    println!("Guess {}",guess);
+
+    let x = 2.0; //f64
+    let y: f32 = 3.0; //f32
+
+    //addition
+    let sum = 5 + 10;
+    //subtraction
+    let difference = 95.5 - 4.3;
+    //
 }
